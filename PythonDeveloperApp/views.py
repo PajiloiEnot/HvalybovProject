f220b89e305ff67db04df7e12abcf7a153d21732
PajiloiEnot <hvalybovroma@mail.ru>
2023-01-13 17:01:20 +0500

    Реализовал логику получения файлов статистики

--- PythonDeveloperApp/views.py
+++ PythonDeveloperApp/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
+from .models import Data
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def demand(request):
-    return render(request, 'demand.html')
+    data = Data.objects.filter(profession_name='python-разработчик')[0]
+    return render(request, 'demand.html', context={'graph': data.demand_graph.url, "table": str(data.demand_table)})
 
 def geography(request):
-    return render(request, 'geography.html')
+    data = Data.objects.filter(profession_name='python-разработчик')[0]
+    return render(request, 'geography.html', context={'graph': data.geography_graph.url, "table": str(data.geography_table)})
 
 def skills(request):
-    return render(request, 'skills.html')
+    data = Data.objects.filter(profession_name='python-разработчик')[0]
+    return render(request, 'skills.html', context={'graph': data.skills_graph.url, "table": str(data.skills_table)})
 
 def last_vacancies(request):
     return render(request, 'last-vacancies.html')
