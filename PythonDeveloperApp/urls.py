ffe60a032ab808f9ee4f1ac8c8ba5b6d6e0145f7
PajiloiEnot <hvalybovroma@mail.ru>
2023-01-13 16:17:33 +0500

    Добавли шаблоны страниц в проект

--- PythonDeveloperApp/urls.py
+++ PythonDeveloperApp/urls.py
@@ -3,4 +3,13 @@ from django.conf.urls.static import static
 from django.urls import path
 from PythonDeveloperApp import views
 
+urlpatterns = [
+    path("", views.main),
+    path("relevance", views.relevance),
+    path("geography", views.geography),
+    path("skills", views.skills),
+    path("last-vacancies", views.last_vacancies)
+]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
